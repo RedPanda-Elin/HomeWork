5c6971b00ecf226e028a2064078f4cb18b471bb9
ElinDi <vivalatanya@gmail.com>
2017-09-08 02:09:15 +0500

    Удовлетворяющий меня вариант домашки, хотя тут нет дефолтного варианта на случай, если введут не число. Но мне не охото это делать через If else. Я помню, что есть более изящное решение, но я его не помню. Завтра может поищу.. ну.. или Сенсей подскажет ^^

--- src/proba_pera.java
+++ src/proba_pera.java
@@ -1,12 +1,41 @@
+import java.util.Scanner;
+
 /**
  * Created by Elin on 28.08.2017.
  */
 public class proba_pera {
     public static void main(String[] args) {
-        StringBuilder hi = new StringBuilder();
-        hi.append("Привет, мир");
-        hi.append(". ");
-        hi.append("Как твои дела?");
-        System.out.println(hi.toString());
+        int first;
+        int second;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Пожалуйста, введите целое число");
+
+            first = scanner.nextInt();
+        System.out.println("Введите второе число для вычислений");
+            second = scanner.nextInt();
+
+//            chek(first, second);
+            System.out.println(chek(first,second));
+        }
+
+
+
+    public static boolean chek (int a, int b) {
+        int summ = a + b;
+        boolean result = false;
+
+        if (10 <= summ) {
+            if (summ <= 20) {
+                result = true;
+
+            }
+        } else {
+            result = false;
+        }
+
+
+
+        return result;
     }
 }
